51ebba65eda02ed1fd2b813323d4fd3c85987685
Pranav-Patani <pranav.patani@gmail.com>
2025-03-25 16:21:00 +0530

    feat: create basic app layout

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
+import { NavLink } from "react-router-dom";
+
 const NavList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +46,20 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">Dashboard</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">Bookings</StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
